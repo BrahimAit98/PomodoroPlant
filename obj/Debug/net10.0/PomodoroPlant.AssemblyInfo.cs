e89d67065f2d24521daba3990e65deda921957d0
Shugri7002 <Shugri7002@hotmail.com>
2025-11-30 03:37:02 +0100

    homepage and onboarding page work

--- obj/Debug/net10.0/PomodoroPlant.AssemblyInfo.cs
+++ obj/Debug/net10.0/PomodoroPlant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PomodoroPlant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740225969d5ea203bdcabd357ad40a96b96deab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992be0c7a97c90812c7998d92e4a2dc2e02f5c4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PomodoroPlant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PomodoroPlant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
